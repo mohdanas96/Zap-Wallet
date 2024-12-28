b1c8a88d6a9a48fb8ad84bb68c6e11f2f94dcab4
Mohammed Anas <mohammedanasdk963@gmail.com>
2024-12-28 20:52:50 +0530

    added prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,9 @@
 import Image, { type ImageProps } from 'next/image'
 import { Button } from '@repo/ui/button'
 import styles from './page.module.css'
+import { PrismaClient } from '@repo/database'
+
+const client = new PrismaClient()
 
 type Props = Omit<ImageProps, 'src'> & {
   srcLight: string
